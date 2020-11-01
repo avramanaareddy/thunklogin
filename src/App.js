2e58989be56f6684e54059be6f795a73f03e2c79
Vennkata <avramanaareddy@yahoo.com>
2020-10-31 21:07:38 -0400

    login

--- src/App.js
+++ src/App.js
@@ -1,60 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {connect} from 'react-redux';
-import {BrowserRouter,Redirect} from 'react-router-dom';
-import LoginForm from './login/LoginForm';
-import {BrowserRouter as Router,Route,Switch,useLocation} from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "react-redux";
+import { BrowserRouter, Redirect } from "react-router-dom";
+import LoginForm from "./login/LoginForm";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation
+} from "react-router-dom";
 
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 
-
-import NoMatch from './NoMatch';
-import LoginThunkForm from './login/LoginThunkForm';
+import NoMatch from "./NoMatch";
+import LoginThunkForm from "./login/LoginThunkForm";
 
 function App(props) {
-  console.log('App Component -->',props,props.store.user);
-  const {error} = props.store.user;
+  console.log("App Component -->", props, props.store.user);
+  const { error } = props.store.user;
   return (
     <div>
+      <div>{error}</div>
       <div>
-        {error}
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <LoginThunkForm />
+            </Route>
+            <PrivateRoute path="/">
+              <Navbar />
+            </PrivateRoute>
+          </Switch>
+        </Router>
       </div>
-      <div>
-    <Router>
-       <Switch>
-          <Route  path="/login"><LoginThunkForm/></Route>
-          <PrivateRoute  path="/"><Navbar/></PrivateRoute> 
-       </Switch>
-
-   </Router>
-   </div>
-   </div> 
+    </div>
   );
 }
-function PrivateRoute({ children, ...rest }){
-  console.log('Helloworld ',children);
-  console.log('User-->',window.localStorage.getItem("user"));
-  console.log(window.localStorage.getItem("user")!=null);
- return ( <Route
-  {...rest}
-  render={({ location }) =>
-    window.localStorage.getItem("user") ? (
-      children
-    ) : (
-      <Redirect
-        to={{
-          pathname: "/login",
-          state: { from: location }
-        }}
-      />
-    )
-  }
-/>
-);
-  
+function PrivateRoute({ children, ...rest }) {
+  console.log("Helloworld ", children);
+  console.log("User-->", window.localStorage.getItem("user"));
+  console.log(window.localStorage.getItem("user") != null);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        window.localStorage.getItem("user") ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
 }
-const mapStateToProps = (state) => {return {store :state}};
-
+const mapStateToProps = (state) => {
+  return { store: state };
+};
 
 export default connect(mapStateToProps)(App);
